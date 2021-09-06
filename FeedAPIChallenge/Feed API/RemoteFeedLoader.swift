5a3e460e42ac9bce4ba20ce5a4b781734cd8461a
Jovanny Espinal <espinaljovanny@icloud.com>
2021-09-06 13:38:48 -0500

    Replaces hardcoded status code with a static constant

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -53,8 +53,10 @@ private struct FeedImageMapper {
 		}
 	}
 
+	private static let STATUS_CODE_200 = 200
+
 	internal static func map(_ data: Data, from response: HTTPURLResponse) -> FeedLoader.Result {
-		guard response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+		guard response.statusCode == STATUS_CODE_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 
